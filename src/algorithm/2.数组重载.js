8799a738577da8c8e8e732513daf11b87fb69ec3
lfp1024 <lfp1024@126.com>
2020-06-28 15:08:16 +0800

    add 嵌套数组重载

---
+++ src/algorithm/2.数组重载.js
@@ -0,0 +1,24 @@
+
+(() => {
+    const arr = [1, [2, [[3, 4], 5], 6], 7]
+
+    let i = 0
+    function reload(arg) {
+        console.log("time = ", i++)
+        let reloadArr = []
+        for (const e of arg) {
+            console.log("e = ", e)
+            if (Array.isArray(e)) {
+                const res = reload(e)
+                reloadArr = reloadArr.concat(res)
+                console.log("reload = ", reloadArr)
+            } else {
+                reloadArr.push(e)
+            }
+        }
+        return reloadArr
+    }
+
+    const res = reload(arr)
+    console.log("Res = ", res)
+})()
