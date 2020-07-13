722db1ec145998ebe47962531fb2139c1b1f5811
lfp1024 <lfp1024@126.com>
2020-07-14 00:31:24 +0800

    add finally and test

---
+++ src/syntax/promise/11.promise.finally.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+const Promise = require("./2.my-promise")
+
+Promise.reject(123).finally(() => {
+    console.log("finally")
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            // res("ok")
+            rej("fail")
+        }, 1000);
+    })
+}).then(result => {
+    console.log("result = ", result)
+}).catch(err => {
+    console.log("err = ", err)
+})
