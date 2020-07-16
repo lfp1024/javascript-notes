3fc8fa6835b567bfaa81037e92a567306708760a
lfp1024 <lfp1024@126.com>
2020-07-16 17:52:20 +0800

    update utils

--- src/syntax/promise/utils.js
+++ src/syntax/promise/utils.js
@@ -2,9 +2,10 @@ const debug = require("debug")
 const path = require("path")
 
 /* 
-开启打印 export DEBUG=DEV:debug:*
-取消打印 unset DEBUG
-*/
+打印所有级别  export DEBUG=DEV:*
+仅打印error  export DEBUG=DEV:error:*
+取消打印     unset DEBUG
+*/  
 function debugGenerator(fileName) {
     return {
         debug: debug(`DEV:debug:${path.basename(fileName)}`),
