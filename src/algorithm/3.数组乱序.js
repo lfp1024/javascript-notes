9eef2536f40ec7a1a52b19b5359b4be0a8682e64
lfp1024 <lfp1024@126.com>
2020-12-08 15:16:57 +0800

    update

--- src/algorithm/3.数组乱序.js
+++ src/algorithm/3.数组乱序.js
@@ -10,10 +10,10 @@ fisher-yates 算法
 
 (() => {
 
-    function outOfOrder(arr: number[]) {
+    function outOfOrder(arr) {
         let m = arr.length - 1;
         while (m > 0) {
-            // 向下取整，自动排除最后一个元素，最后一个随机数一定是0
+            // 向下取整，最后一个随机数一定是0，自动排除最后一个元素【上面的m本身已经减1，再向下取整，就无法得到最后一个元素的下标。如果不减1，则可取到】
             const randomIndex = Math.floor(Math.random() * m);
             console.log(`index is ${randomIndex}`);
             // 随机位置的元素arr[index] 跟 当前元素arr[m] 交换
@@ -27,6 +27,14 @@ fisher-yates 算法
     }
 
     const arr = [1, 2, 3, 4]
-    const res = outOfOrder(arr)
-    console.log(res.toString())
+    // const res = outOfOrder(arr)
+    // console.log(res.toString())
+
+
+    const newArr = [];
+    const len = arr.length;
+    for (let j = 0; j < len; j++) {
+        newArr.push(arr.splice(Math.random() * arr.length, 1)[0]); // 两个数组有可能相同
+    }
+    console.log(newArr);
 })()
--- src/syntax/statements/switch.js
+++ src/syntax/statements/switch.js
@@ -1,13 +1,23 @@
 ((type) => {
 
+    // switch (type) {
+    //     case 'a':
+    //         console.log('type = ', type);
+    //         break;
+    //     default:
+    //         console.log('type = ', type);
+    //         return; // 结束整个方法
+    // }
+    // console.log('after return');
+
     switch (type) {
-        case 'a':
+        case 'a':   // 也会走到 case 'b'
+        case 'b':
             console.log('type = ', type);
             break;
         default:
-            console.log('type = ', type);
-            return; // 结束整个方法
+            console.log('no');
+            break;
     }
-    console.log('after return');
 
 })('b')
