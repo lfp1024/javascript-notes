41120f3fb8d6480fb44bc68e8fcfc00f7e78a8cd
lfp1024 <lfp1024@126.com>
2021-01-05 16:21:38 +0800

    for in await 测试

--- src/syntax/loop/for-in.js
+++ src/syntax/loop/for-in.js
@@ -1,7 +1,6 @@
 'use strict';
 
-
-(() => {
+function forin(params) {
     //         key1 value1(key2 value2)
     let obj = { 1: { 12: 1, 13: 0 } };
     // let obj = {}; // 不会进入
@@ -13,4 +12,35 @@
             console.log('value2 = ', obj[key1][key2]);
         }
     }
-})();
+}
+
+async function forinAwait() {
+
+    let obj = {
+        name: 'lfo',
+        age: 23
+    };
+    console.log('start');
+
+    for (const key in obj) {
+        await new Promise(res => setTimeout(() => {
+            console.log(Date.now(), key, obj[key]);
+            res();
+        }, 1000))
+        console.log(Date.now(),'haha');
+    }
+
+    // ['a', 'b'].forEach(async e => {
+    //     await new Promise(res => setTimeout(() => {
+    //         console.log(Date.now(), e);
+    //         res();
+    //     }, 1000))
+    //     console.log(Date.now(),'hehe');
+    // });
+
+    console.log('end');
+}
+
+
+
+forinAwait();
