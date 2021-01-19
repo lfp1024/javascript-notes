954d278d29a2b48472267d64af266308c7aabc59
lfp1024 <lfp1024@126.com>
2021-01-19 15:26:27 +0800

    async while

--- src/syntax/loop/while.js
+++ src/syntax/loop/while.js
@@ -1,8 +1,9 @@
-(() => {
+(async () => {
     console.log('start')
     let i = 0
-    while (true) {
+    while (true) { // 阻塞
         i++;
+        await new Promise(res => setTimeout(res, 1000 * i)); // 串行
         console.log('i = ', i)
         if (i > 2) break;
     }
