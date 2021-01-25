75af3319a61561d712a84692473079afd6dd09a0
lfp1024 <lfp1024@126.com>
2021-01-25 13:49:26 +0800

    for of

---
+++ src/syntax/loop/for-of.js
@@ -0,0 +1,24 @@
+// const foo = [
+//     'apples', 'oranges', 'pears'
+// ]
+// console.log('entries = ', foo.entries());
+// for (const thing of foo.entries()) {
+//     console.log(thing)
+// }
+
+// [ 0, 'apples' ]
+// [ 1, 'oranges' ]
+// [ 2, 'pears' ]
+
+const foo = {
+    'apples': { name: '苹果', weight: 2 },
+    'pears': { name: '梨', weight: 3 }
+}
+// entries =  [ [ 'apples', 'oranges' ], [ 'pears', 'prunes' ] ]
+console.log('entries = ', Object.entries(foo));
+for (const [key, value] of Object.entries(foo)) { // 遍历的颗粒是数组，利用数组的解构赋值
+    console.log(key, ':', value)
+}
+for (const [key, { name, weight }] of Object.entries(foo)) { // 同时数组解构和对象解构
+    console.log(key, ':', name, weight)
+}
