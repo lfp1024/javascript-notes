5670b15eabceb3b1272cb2038ad4efb994f405eb
lfp1024 <lfp1024@126.com>
2021-02-17 19:16:49 +0800

    exec 添加 g 标志

--- src/syntax/regexp/demo.js
+++ src/syntax/regexp/demo.js
@@ -29,5 +29,6 @@ const str2 = 'lazada#MY#5474 amazon#VN#1234';
 // 设置 global 标志位（如 /foo/g）
 // exec => RegExp 对象是有状态的，会将上次成功匹配后的位置记录在 lastIndex 属性中。exec可通过该属性对单个字符串中的多个匹配结果进行**逐条遍历**（包括捕获到的匹配）
 // match => 不会记录上次匹配后的位置，而是返回所有匹配到的结果数组，不返回捕获组
-// console.log('match2 = ', str2.match(regexp2), regexp2.lastIndex);
-// console.log('exec2 = ', regexp2.exec(str2), regexp2.lastIndex);
+console.log('match2 = ', str2.match(regexp2), regexp2.lastIndex);
+console.log('exec2 = ', regexp2.exec(str2), regexp2.lastIndex);
+console.log('exec3 = ', regexp2.exec(str2), regexp2.lastIndex); // 遍历第二个
