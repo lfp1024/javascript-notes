25243593e928982c73a4799f76e1ab50c3a801d4
lfp1024 <lfp1024@126.com>
2021-02-21 10:59:34 +0800

    for of

--- src/syntax/loop/for-of.js
+++ src/syntax/loop/for-of.js
@@ -10,15 +10,27 @@
 // [ 1, 'oranges' ]
 // [ 2, 'pears' ]
 
-const foo = {
-    'apples': { name: '苹果', weight: 2 },
-    'pears': { name: '梨', weight: 3 }
+// const foo = {
+//     'apples': { name: '苹果', weight: 2 },
+//     'pears': { name: '梨', weight: 3 }
+// }
+
+// console.log('entries = ', Object.entries(foo));
+// for (const [key, value] of Object.entries(foo)) { // 遍历的颗粒是数组，利用数组的解构赋值
+//     console.log(key, ':', value)
+// }
+// for (const [key, { name, weight }] of Object.entries(foo)) { // 同时数组解构和对象解构
+//     console.log(key, ':', name, weight)
+// }
+
+const bar = {
+    'xm': 12,
+    'xh': 13,
 }
-// entries =  [ [ 'apples', 'oranges' ], [ 'pears', 'prunes' ] ]
-console.log('entries = ', Object.entries(foo));
-for (const [key, value] of Object.entries(foo)) { // 遍历的颗粒是数组，利用数组的解构赋值
-    console.log(key, ':', value)
+
+for (const [name, age] of Object.entries(bar)) {
+    console.log('name:age', name, age);
 }
-for (const [key, { name, weight }] of Object.entries(foo)) { // 同时数组解构和对象解构
-    console.log(key, ':', name, weight)
-}
+
+
+
